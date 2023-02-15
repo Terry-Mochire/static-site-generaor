d28bac73bca9ac512122e23774efbfb73e11ea82
Terry <terrybmochire@gmail.com>
2023-02-15 14:42:56 +0300

    Test helper function to Render Jinja2 template with data.

--- test_generator.py
+++ test_generator.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 import os
 import tempfile
 
-from generator import markdown_to_html
+from generator import markdown_to_html, render_template
 
 
 class Test(TestCase):
@@ -24,4 +24,32 @@ class Test(TestCase):
 
         # Delete the temporary files
         os.remove(input_file.name)
+        os.remove(output_file.name)
+
+    def test_render_template(self):
+        # Create a temporary input file with .html extension in the templates Folder with some markdown content
+        with tempfile.NamedTemporaryFile(mode='w', delete=False, dir='templates/', suffix='.html') as template_file:
+            template_file.write('<h1>{{ title }}</h1>\n<p>{{ content}}</p>')
+
+            template_file_name = template_file.name.split('/')[-1]
+            print(template_file_name)
+            template_file.flush()
+
+            # Create a temporary output file
+            with tempfile.NamedTemporaryFile(mode='w', delete=False) as output_file:
+                output_file.flush()
+
+                # Define context
+                context = {'title': 'Hello World', 'content': 'This is test content'}
+
+                # Call the function
+                render_template(template_file_name, output_file.name, context)
+
+                # Check the output file
+                expected_html = '<h1>Hello World</h1>\n<p>This is test content</p>'
+                with open(output_file.name, 'r') as f:
+                    self.assertEqual(f.read(), expected_html)
+
+        # Delete the temporary files
+        os.remove(template_file.name)
         os.remove(output_file.name)
