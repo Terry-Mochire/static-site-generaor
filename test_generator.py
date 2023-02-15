4316968e67417f23090dd9254daf22f54eff8e33
Terry <terrybmochire@gmail.com>
2023-02-15 20:26:00 +0300

    Add test for generate_site method - fails.

--- test_generator.py
+++ test_generator.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 import os
 import tempfile
 
-from generator import markdown_to_html, render_template
+from generator import markdown_to_html, render_template, generate_site
 
 
 class Test(TestCase):
@@ -52,3 +52,28 @@ class Test(TestCase):
         # Delete the temporary files
         os.remove(template_file.name)
         os.remove(output_file.name)
+
+    def test_generate_site(self):
+        # Create a temporary input directory with some markdown files
+        with tempfile.TemporaryDirectory() as input_directory:
+            with tempfile.NamedTemporaryFile(mode='w', delete=False, dir=input_directory, suffix='.md') as input_file:
+                input_file.write('# Hello World')
+                input_file.flush()
+
+        # Create a temporary output directory
+        with tempfile.TemporaryDirectory() as output_directory:
+            # Call the function
+            generate_site(input_directory, output_directory)
+
+            # Check the output directory
+            self.assertTrue(os.path.exists(output_directory + '/static/'))
+            self.assertTrue(os.path.exists(output_directory + '/hello-world.html'))
+
+            # Check the output file
+            with open(output_directory + '/hello-world.html', 'r') as f:
+                self.assertEqual(f.read(), '<h1>Hello World</h1>')
+
+        # Delete the temporary files
+        os.remove(input_file.name)
+        os.remove(output_directory + '/hello-world.html')
+        os.remove(output_directory + '/static/')
