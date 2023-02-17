e7fb15c2b916c0ae16fc37ec2f3436b484c04c41
Terry <terrybmochire@gmail.com>
2023-02-17 23:43:43 +0300

    Add all required paths.

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 import os
 
-from flask import Flask, request, flash, redirect, render_template
+from flask import Flask, request, redirect, render_template, abort
+from jinja2 import FileSystemLoader, ChoiceLoader
 from werkzeug.utils import secure_filename
 
+import generator
+
 app = Flask(__name__)
+template_dirs = ['output/templates/', 'templates/']
+loaders = [FileSystemLoader(d) for d in template_dirs]
+loader = ChoiceLoader(loaders)
+app.jinja_loader = loader
 UPLOAD_FOLDER = 'uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
@@ -26,10 +34,56 @@ def upload():
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-        return "File uploaded successfully"
+        return redirect('/load')
 
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+@app.route('/load', methods=['GET'])
+def load():
+    if request.method == 'GET':
+        generator.generate_site('uploads/', 'output/')
+        return redirect('/index.html')
+
+
+@app.route('/index.html', methods=['GET'])
+def index_html():
+    if request.method == 'GET':
+        try:
+            return render_template('index.html')
+        except:
+            abort(404)
+
+
+@app.route('/about.html', methods=['GET'])
+def about_html():
+    if request.method == 'GET':
+        try:
+            return render_template('about.html')
+        except:
+            abort(404)
+
+
+@app.route('/contact.html', methods=['GET'])
+def contact_html():
+    if request.method == 'GET':
+        try:
+            return render_template('contact.html')
+        except:
+            abort(404)
+
+
+@app.route('/articles.html', methods=['GET'])
+def articles_html():
+    if request.method == 'GET':
+        try:
+            return render_template('articles.html')
+        except:
+            abort(404)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html'), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
